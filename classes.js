5a19bbe238feedd69004e4592f7bf44701a1aa9c
alireza <easazade@gmail.com>
2018-07-07 18:30:32 +0430

    update

--- classes.js
+++ classes.js
@@ -1,4 +1,7 @@
-"use strict"
+"use strict";
+
+//setting value for the Window object
+self.name = "window_property_name";
 
 class User {
 
@@ -15,12 +18,23 @@ class User {
     }
 
     getListOfBooks() {
+        //how ever there is no point to write getters and setters for public variables
+        //just wanted to write a function
         return this.books;
+    }
+
+    getName() {
+        //TODO if we return just name instead of this.name the name of the window's name property
+        //TODO will be returned note that window has a name property
+        //TODO by default which does not have a name in the
+        //TODO code above we're just giving it a value for the sake of the test
+        return this.name;
     }
 
 }
 
 
-let user = new User("ali","93u80d3h290ue",["math","literature","physics"]);
+let user = new User("ali", "93u80d3h290ue", ["math", "literature", "physics"]);
 console.log(user.getListOfBooks());
 console.log(user.getLocalVar());
+
