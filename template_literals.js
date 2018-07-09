2f611f95a04085c44344ebef6819a458c9cde47e
alireza <easazade@gmail.com>
2018-07-09 19:53:21 +0430

    update

---
+++ template_literals.js
@@ -0,0 +1,16 @@
+
+
+function getDate(){
+    return "1400/01/26";
+}
+
+const name = "alireza";
+
+const message = 
+` hello ${name},
+    it has been very nice of you to send me a letter.
+    i'm looking forward to see you in the comming week on ${getDate()}
+    
+    thanks shapoor gholam reza`;
+
+console.log(message);    
