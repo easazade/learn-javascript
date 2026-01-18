e179de5b7fb7eab4e74f30b89ddcfb8fd1eb4b4d
alireza easazade <easazade@gmail.com>
2026-01-19 01:18:54 +0330

    feat(basics): Update examples

--- basics/src/classes.js
+++ basics/src/classes.js
@@ -1,7 +1,11 @@
 'use strict';
 
-//setting value for the Window object
-self.name = 'window_property_name';
+// Setting a value on the global object (browser: window, Node: globalThis)
+globalThis.name = 'global_property_name';
+console.log(globalThis);
+console.log(`Checking: ${globalThis.name}`);
+// If you specifically want to access `window`, only do it in a browser:
+// if (typeof window !== 'undefined') window.name = 'window_property_name';
 
 class User {
   constructor(name, id, books) {
@@ -11,6 +15,9 @@ class User {
     this.id = id;
     this.name = name;
     this.books = books;
+    // Instance-specific method created per object
+    // Uses closure to access private constructor-local variables
+    // Constructor methods are private closures; class methods are shared behavior.
     this.getLocalVar = function () {
       return localVar;
     };
@@ -23,10 +30,10 @@ class User {
   }
 
   getName() {
-    //TODO if we return just name instead of this.name the name of the window's name property
-    //TODO will be returned note that window has a name property
-    //TODO by default which does not have a name in the
-    //TODO code above we're just giving it a value in above for the sake of the test
+    // NOTE: In a browser, `window` has a built-in `name` property.
+    // But in modern JS (and especially in strict mode), returning `name` (without `this.`)
+    // does NOT read from the global object; it throws ReferenceError unless a local `name` exists.
+    // Always use `this.name` here.
     return this.name;
   }
 }
