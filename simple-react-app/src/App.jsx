538c394d8f19adbad5f1d4da99acc0d8bb1415c6
alireza easazade <easazade@gmail.com>
2026-03-13 20:53:26 +0330

    commit last changes

--- simple-react-app/src/App.jsx
+++ simple-react-app/src/App.jsx
@@ -1,15 +1,6 @@
 const Card = ({ title }) => {
   return (
-    <div
-      style={{
-        border: '1px solid #4b5362',
-        padding: '20px',
-        margin: '10px',
-        backgroundColor: '#31363f',
-        borderRadius: '10px',
-        minHeight: '100px',
-      }}
-    >
+    <div className="card">
       <h2>{title}</h2>
     </div>
   )
